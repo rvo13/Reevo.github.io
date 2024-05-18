a5e9b964c7c63d6529b1dc31acef6fa11ae44365
rvo13 <109659413+rvo13@users.noreply.github.com>
2024-05-19 01:31:05 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+console.log('test')
+
+function menuu() {
+  menu.style.display = 'inline-block'
+  
+}
+
+function tutup() {
+  menu.style.display = 'none'
+}
+
+function ig() {
+  window.location.href = 'https://www.instagram.com/reevo.adnan13?igsh=cnNqMGQ3ODZoNWdq'
+}
